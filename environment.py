74e113b0fd2829126939df499e9b5984928baed2
FrostDad <54032077+FrostDad-YT@users.noreply.github.com>
2020-11-03 14:31:29 +0700

    Update environment.py

--- environment.py
+++ environment.py
@@ -314,8 +314,7 @@ def step(action):
 def exploration_rate(n, min_rate=0.05):
     """ Метод для вычисления коэффициента 'любопытства'.
         Чем дольше мы обучаемся, тем больше мы опираемся на политику и меньше на рандом."""
-    if random.uniform(0, 1) >= max(min_rate,
-                                   min(1, 1.0 - math.log10((n + 1) / 10))):  # Чем меньше число, тем больший рандом
+    if random.uniform(0, 1) >= max(min_rate, min(1, 1.0 - math.log10((n + 1) / 10))):  # Чем меньше число, тем больший рандом
         return 'action'
     else:
         return 'random'
